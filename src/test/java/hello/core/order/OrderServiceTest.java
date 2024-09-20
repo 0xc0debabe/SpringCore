aae1636ce8b4669d8c84d921b088f77ab18fac47
0xc0debabe <kongminoo@naver.com>
2024-09-20 15:59:18 +0900

    [9/20] SpringCore
    Member, Order 엔티티, 서비스, 테스트 구현

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //when
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        //then
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+     }
+
+}
